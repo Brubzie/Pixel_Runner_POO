c66d4b7bf2cbcb9a8f72ada1f50a417e5fcf72cb
Bruno Alves <127157201+Brubzie@users.noreply.github.com>
2024-08-06 17:49:13 -0300

    Update colors.py

--- colors.py
+++ colors.py
@@ -1,3 +1,4 @@
+# Cores básicas
 BLACK  = (0, 0,0)
 WHITE  = (255, 255, 255)
 RED    = (255, 0, 0)
@@ -6,6 +7,7 @@ BLUE   = (0,0,255)
 GREEN = (0,255,0)
 
 text_color = (64, 64, 64)
+title_color = (111, 196, 169)
 box_color = '#c0e8ec'
 game_over_bg = (94, 129, 162)
 points_text_color = (111, 196, 169)
